85179b526c9578d61bb4adbc2f3d62000c1e668a
Amir Ahmed Siddiqui <113359516+Am1rAhmed@users.noreply.github.com>
2023-12-04 21:36:17 +0530

    Add files via upload

---
+++ Boston_house_price_prediction/scores.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def print_scores(scores):
+    print("scores : ",scores)
+    print("mean : ",scores.mean())
+    print("Standard deviation : ",scores.std())
+
